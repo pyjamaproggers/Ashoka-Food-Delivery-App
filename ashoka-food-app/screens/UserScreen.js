4613ca093ec47bbd6ed9a19b7e99cb1a2c2a69e2
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-06-13 13:02:48 +0530

    Fixed retarded back button not being aligned left

--- ashoka-food-app/screens/UserScreen.js
+++ ashoka-food-app/screens/UserScreen.js
@@ -1,8 +1,8 @@
 import React, { useLayoutEffect, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { View, Text, Image, TextInput, ScrollView, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { ArrowLeftIcon, ClockIcon, MapPinIcon, BoltIcon} from 'react-native-heroicons/solid';
+import { ArrowLeftIcon } from 'react-native-heroicons/solid';
 
 export default function UserScreen() {
   const { params: { user } } = useRoute();
@@ -12,8 +12,8 @@ export default function UserScreen() {
       flex: 1,
       justifyContent: 'flex-start',
       alignItems: 'center',
-      borderColor: 'gray',
-      borderWidth: 1,
+    //   borderColor: 'gray',
+    //   borderWidth: 1,
     },
     userPic: {
       width: 90,
@@ -22,25 +22,27 @@ export default function UserScreen() {
     },
     nameEmailPhotoContainer: {
       width: '90%',
-      height: '15%',
+      height: 150,
       flexDirection: 'row',
-      justifyContent: 'flex-start', // Updated to 'flex-start'
+      justifyContent: 'space-between',
       alignItems: 'center',
-      paddingHorizontal:"5%",
-      paddingBottom:0,
-      marginTop:"5%"
+      paddingHorizontal: 20,
+      marginTop: 10,
+      backgroundColor: 'white',
+      borderRadius: 10,
     },
     nameText: {
       fontWeight: 'bold',
-      paddingBottom:"2%",
+      paddingBottom: 2,
+      fontSize: 20,
     },
     emailText: {
       fontSize: 10,
     },
-    backButton: {
-        position: 'absolute',
-        left: 10,
-      },
+    backButton:{
+        width:"10%",
+        marginLeft:20
+    }
   });
 
   useLayoutEffect(() => {
@@ -52,33 +54,28 @@ export default function UserScreen() {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={styles.container} className='shadow'>
-
-
-        {/* Go back Button */}
-        <View className="">
-        <TouchableOpacity onPress={navigation.goBack} className="p-2 bg-gray-100 rounded-full" >
-            <ArrowLeftIcon size={20} color="black"/>
-        </TouchableOpacity>
-        </View>
-
+    <SafeAreaView style={{ flex: 1 }} className="shadow">
+      {/* Go back Button */}
+      <TouchableOpacity onPress={navigation.goBack} className="p-2 bg-gray-100 rounded-full items-center" style={styles.backButton}>
+        <ArrowLeftIcon size={20} color="black" />
+      </TouchableOpacity>
+      <View style={styles.container}>
         {/* First container of name, email, and photo */}
-      <View style={styles.nameEmailPhotoContainer} className="bg-white rounded-xl ">
-        <View style={{ flex: 1 }}>
-          <Text style={styles.nameText} className="text-xl">{user.given_name}</Text>
-          <Text style={styles.emailText}>{user.email}</Text>
+        <View style={styles.nameEmailPhotoContainer}>
+          <View>
+            <Text style={styles.nameText}>{user.given_name}</Text>
+            <Text style={styles.emailText}>{user.email}</Text>
+          </View>
+          <View>
+            {user.picture.length !== 0 ? (
+              <Image style={styles.userPic} source={{ uri: user.picture }} />
+            ) : (
+              <Image style={styles.userPic} source={{ uri: user.picture }} />
+            )}
+          </View>
         </View>
-        <View>
-          {user.picture.length !== 0 ? (
-            <Image style={styles.userPic} source={{ uri: user.picture }} />
-          ) : (
-            <Image style={styles.userPic} source={userPic} />
-          )}
-        </View>
-      </View>
-      {/* View Order */}
-      <View>
-
+        {/* View Order */}
+        <View></View>
       </View>
     </SafeAreaView>
   );
