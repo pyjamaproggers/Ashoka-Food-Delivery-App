b6fdfc6b3d1e4c719821f0e666f9f88e898adfbe
iamaryanyadavv <aryanyadav1601@gmail.com>
2023-06-26 15:21:38 +0530

    installed native base component lib, added restaurant cards loading skeleton to home screen

--- ashoka-food-app/App.js
+++ ashoka-food-app/App.js
@@ -12,25 +12,28 @@ import UserScreen from './screens/UserScreen';
 import PhoneAuthScreen from './screens/PhoneAuthScreen';
 import { setupURLPolyfill } from "react-native-url-polyfill"
 import CartScreen from './screens/CartScreen';
+import {NativeBaseProvider} from 'native-base'
 
 setupURLPolyfill()
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return ( 
-    <NavigationContainer> 
-      <Provider store={store}>
-      <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} initialParams={{logout: null}} />
-      <Stack.Screen name="PhoneAuth" component={PhoneAuthScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      <Stack.Screen name="UserScreen" component={UserScreen} />
-      <Stack.Screen name="Cart" component={CartScreen} />
-      </Stack.Navigator>
-      </Provider>
-    </NavigationContainer>
-  );
+    return (
+		<NativeBaseProvider>
+			<NavigationContainer>
+				<Provider store={store}>
+					<Stack.Navigator>
+						<Stack.Screen name="Login" component={Login} initialParams={{ logout: null }} />
+						<Stack.Screen name="PhoneAuth" component={PhoneAuthScreen} />
+						<Stack.Screen name="Home" component={HomeScreen} />
+						<Stack.Screen name="Restaurant" component={RestaurantScreen} />
+						<Stack.Screen name="UserScreen" component={UserScreen} />
+						<Stack.Screen name="Cart" component={CartScreen} />
+					</Stack.Navigator>
+				</Provider>
+			</NavigationContainer>
+		</NativeBaseProvider>
+    );
 }
 
 
--- ashoka-food-app/screens/Restaurants.js
+++ ashoka-food-app/screens/Restaurants.js
@@ -6,213 +6,257 @@ import HomeImage1 from '../assets/HomeImage1.jpeg';
 import client from '../sanity'
 import { useLayoutEffect } from 'react'
 import Styles from '../components/Styles'
+import { HStack, Skeleton, VStack } from 'native-base';
+
 const Restaurants = () => {
-
-    // const DRestaurants = [
-    //     {
-    //         id:7,
-    //         title:"Roti Boti",
-    //         image: Grey,
-    //         timing: "6am To 12am",
-    //         genre: "Indian & Chinese",
-    //         description: "Best Mix Of Indian & Chinese",
-    //         location: "Opposite Tennis Court"
-    //     },
-    //     {
-    //         id:1,
-    //         title:"The Hunger Cycle",
-    //         image: Grey,
-    //         timing: "6am To 12am",
-    //         genre: "Fast Food",
-    //         description: "EPIC MUNCHIES 24 X 7",
-    //         location: "Mess - G Floor"
-    //     },
-    //     {
-    //         id:2,
-    //         title:"Chicago Pizza",
-    //         image: Grey,
-    //         timing: "6am To 12am",
-    //         genre: "Fast Food",
-    //         description: "Big Slices, Really Fast!",
-    //         location: "Next To Mess"
-    //     },
-    //     {
-    //         id:3,
-    //         title:"Subway",
-    //         image: Grey,
-    //         timing: "6am To 12am",
-    //         genre: "Fast Food",
-    //         description: "Eat Fresh",
-    //         location: "Mess - 1st Floor"
-    //     },
-    //     {
-    //         id:4,
-    //         title:"Dhaba",
-    //         image: Grey,
-    //         timing: "6am To 12am",
-    //         genre: "Indian",
-    //         description: "Classic Indian Dhaba",
-    //         location: "Next To Frisbee Field"
-    //     },
-    //     {
-    //       id:5,
-    //       title:"Chaat Stall",
-    //       image: Grey,
-    //       timing: "6am To 12am",
-    //       genre: "Chaat",
-    //       description: "From Gol Gappe To Rolls!",
-    //       location: "Next To Tennis Court"
-    //   },
-    //   {
-    //     id:6,
-    //     title:"Rasananda",
-    //     image: Grey,
-    //     timing: "6am To 12am",
-    //     genre: "Snacks",
-    //     description: "Late Night Snacks Joint",
-    //     location: "Next To Tennis Court"
-    //   },
-    // ]
-    
     const [DRestaurants, setDRestaurants] = useState([]);
-
-    const NDRestaurants = [
-    {
-      id:7,
-      title:"Dosai",
-      image: Grey,
-      timing: "6am To 12am",
-      genre: "South Indian",
-      description: "Authentic South Indian Food Joint",
-      location: "Next To Frisbee Field"
-    },
-    {
-      id:8,
-      title:"Chai Shai",
-      image: Grey,
-      timing: "6am To 12am",
-      genre: "Breakfast",
-      description: "Bread, Eggs & Cakes!",
-      location: "Mess Ground Floor"
-    },
-    {
-      id:9,
-      title:"Fuelzone",
-      image: Grey,
-      timing: "6am To 12am",
-      genre: "Fast Food",
-      description: "Coffee, Shakes & More!",
-      location: "Mess Ground Floor"
-    },
-    {
-      id:10,
-      title:"Nescafe",
-      image: Grey,
-      timing: "6am To 12am",
-      genre: "Coffee & Snacks",
-      description: "Classic Nescafe Joint",
-      location: "Next To AC02"
-    },
-    {
-      id:11,
-      title:"Amul",
-      image: Grey,
-      timing: "6am To 12am",
-      genre: "Dairy",
-      description: "Classic Amul Joint",
-      location: "Next To AC02"
-    },
-    ]
-
+    const [NDRestaurants, setNDRestaurants] = useState([]);
+    const [Fetching, setFetching] = useState()
     const colorScheme = useColorScheme();
 
-    useLayoutEffect(()=>{
+    useLayoutEffect(() => {
     }, [colorScheme])
 
     useEffect(() => {
-      const query = `*[_type == "restaurant"]
-      {description, location,
+        setFetching(true)
+        const query = `*[_type == "restaurant"]
+        {description, location, delivery,
         name, image, genre, timing, 
         dishes[]->{name, Veg_NonVeg, Price, image, Menu_category}}`;
-    
-      client
-        .fetch(query)
-        .then((data) => {
-          console.log('Data:', data); // Log the received data for inspection
-          setDRestaurants(data);
-        })
-        .catch((error) => {
-          console.log('Error:', error); // Log any errors that occur
-        });
+
+        window.setTimeout(() => {
+            client
+                .fetch(query)
+                .then((data) => {
+                    var drest = []
+                    var ndrest = []
+                    var i
+                    var totalD = 0
+                    var totalND = 0
+                    for (i = 0; i < data.length; i++) {
+                        if (data[i].delivery == 'Yes') {
+                            totalD += 1
+                        }
+                        else {
+                            totalND += 1
+                        }
+                    }
+
+                    if (DRestaurants.length != totalD) {
+                        for (i = 0; i < data.length; i++) {
+                            if (data[i].delivery == 'Yes') {
+                                drest.push(data[i])
+                            }
+                        }
+                    }
+
+                    if (NDRestaurants.length != totalND) {
+                        for (i = 0; i < data.length; i++) {
+                            if (data[i].delivery == 'No') {
+                                ndrest.push(data[i])
+                            }
+                        }
+                    }
+                    setDRestaurants(drest)
+                    setNDRestaurants(ndrest)
+                    setFetching(false)
+                })
+                .catch((error) => {
+                    console.log('Error:', error); // Log any errors that occur
+                });
+        }, 3000)
     }, []);
-    
-console.log(DRestaurants);
-  return (
-    <ScrollView >
-      <View className=' w-11/12 h-52 self-center mt-2 mb-2 rounded-full shadow-md'>
-        <Image source={HomeImage1} style={{width: '100%', height: '100%', borderRadius: 15,}} />
-      </View>
 
-      <View className='mt-5 border-t' style={[colorScheme=='light'? Styles.LightHomeAdlibBorder : Styles.DarkHomeAdlibBorder]}  >
-        <Text 
-        className="text-center font-normal text-xs mx-28 mt-3 -top-5" 
-        style={[colorScheme=='light'? Styles.LightHomeAdlib : Styles.DarkHomeAdlib]} 
-        >
-          WHAT'S ON YOUR MIND?
-        </Text>
-      </View>
+    console.log(DRestaurants);
+    return (
+        <ScrollView >
+            <View className=' w-11/12 h-52 self-center mt-2 mb-2 rounded-full shadow-md'>
+                <Image source={HomeImage1} style={{ width: '100%', height: '100%', borderRadius: 15, }} />
+            </View>
 
-      {/* <RestaurantCards name="The Hunger Cycle" image={THC}/>
-      <RestaurantCards name="Chicago Pizza" image={ChicagoPizzaIcon}/>
-      <RestaurantCards name="Subway" image={SubwayIcon}/>
-      <RestaurantCards name="Dosai" image={Dosai}/>
-      <RestaurantCards name="Dhaba" image={Dhaba}/> */}
-      
-      {
-        DRestaurants.map((restaurant)=>
-        (
-            <RestaurantCards 
-            // key={restaurant.id}
-            // id={restaurant.id}
-            image={restaurant["image"]}
-            title={restaurant["name"]}
-            timing={restaurant["timing"]}
-            genre={restaurant["genre"]}
-            location={restaurant["location"]}
-            description={restaurant["description"]}
-            dishes={restaurant["dishes"]}/>
-        ))
-      }
+            <View className='mt-5 border-t' style={[colorScheme == 'light' ? Styles.LightHomeAdlibBorder : Styles.DarkHomeAdlibBorder]}  >
+                <Text
+                    className="text-center font-normal text-xs mx-28 mt-3 -top-5"
+                    style={[colorScheme == 'light' ? Styles.LightHomeAdlib : Styles.DarkHomeAdlib]}
+                >
+                    WHAT'S ON YOUR MIND?
+                </Text>
+            </View>
 
-      <View className=' w-11/12 h-48 self-center mt-4 mb-2 rounded-full shadow-md'>
-        <Image source={Grey} style={{width: '100%', height: '100%', borderRadius: 15,}} />
-      </View>
-      
-      <View className='mt-5 border-t' style={[colorScheme=='light'? Styles.LightHomeAdlibBorder : Styles.DarkHomeAdlibBorder]}  >
-        <Text 
-        className="text-center font-normal text-xs mx-24 mt-3 -top-5" 
-        style={[colorScheme=='light'? Styles.LightHomeAdlib : Styles.DarkHomeAdlib]} 
-        >
-          TAKE A LOOK AT THESE MENUS!
-        </Text>
-      </View>
+            {/* Loading Skeleton */}
+            {Fetching &&
+                <View className='mb-3 mx-4 flex-row '>
+                    <Skeleton w='24' h='24' rounded='lg' startColor='#1f1f1f' style={{ marginRight: 16 }} />
+                    <VStack flex="3" space="2">
+                        <Skeleton h='3' rounded='full' w='50%' startColor='#1f1f1f' />
+                        <HStack space="2" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                    </VStack>
+                </View>
+            }
 
-      {/* {
-        NDRestaurants.map((restaurant)=>
-        (
-            <RestaurantCards 
-            key={restaurant.id}
-            id={restaurant.id}
-            image={restaurant.image}
-            title={restaurant.title}
-            timing={restaurant.timing}
-            genre={restaurant.genre}
-            location={restaurant.location}
-            description={restaurant.description}/>
-        ))
-      } */}
-    </ScrollView>
-  )
+            {Fetching &&
+                <View className='mb-3 mx-4 flex-row '>
+                    <Skeleton w='24' h='24' rounded='lg' startColor='#1f1f1f' style={{ marginRight: 16 }} />
+                    <VStack flex="3" space="2">
+                        <Skeleton h='3' rounded='full' w='50%' startColor='#1f1f1f' />
+                        <HStack space="2" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                    </VStack>
+                </View>
+            }
+
+            {Fetching &&
+                <View className='mb-3 mx-4 flex-row '>
+                    <Skeleton w='24' h='24' rounded='lg' startColor='#1f1f1f' style={{ marginRight: 16 }} />
+                    <VStack flex="3" space="2">
+                        <Skeleton h='3' rounded='full' w='50%' startColor='#1f1f1f' />
+                        <HStack space="2" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                    </VStack>
+                </View>
+            }
+
+            {!Fetching &&
+                DRestaurants.map((restaurant) =>
+                (
+                    <RestaurantCards
+                        // key={restaurant.id}
+                        // id={restaurant.id}
+                        image={restaurant["image"]}
+                        title={restaurant["name"]}
+                        timing={restaurant["timing"]}
+                        genre={restaurant["genre"]}
+                        location={restaurant["location"]}
+                        description={restaurant["description"]}
+                        dishes={restaurant["dishes"]} />
+                ))
+            }
+
+            <View className=' w-11/12 h-48 self-center mt-4 mb-2 rounded-full shadow-md'>
+                <Image source={Grey} style={{ width: '100%', height: '100%', borderRadius: 15, }} />
+            </View>
+
+            <View className='mt-5 border-t' style={[colorScheme == 'light' ? Styles.LightHomeAdlibBorder : Styles.DarkHomeAdlibBorder]}  >
+                <Text
+                    className="text-center font-normal text-xs mx-24 mt-3 -top-5"
+                    style={[colorScheme == 'light' ? Styles.LightHomeAdlib : Styles.DarkHomeAdlib]}
+                >
+                    TAKE A LOOK AT THESE MENUS!
+                </Text>
+            </View>
+
+            {/* Loading Skeleton */}
+            {Fetching &&
+                <View className='mb-3 mx-4 flex-row '>
+                    <Skeleton w='24' h='24' rounded='lg' startColor='#1f1f1f' style={{ marginRight: 16 }} />
+                    <VStack flex="3" space="2">
+                        <Skeleton h='3' rounded='full' w='50%' startColor='#1f1f1f' />
+                        <HStack space="2" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                    </VStack>
+                </View>
+            }
+
+            {Fetching &&
+                <View className='mb-3 mx-4 flex-row '>
+                    <Skeleton w='24' h='24' rounded='lg' startColor='#1f1f1f' style={{ marginRight: 16 }} />
+                    <VStack flex="3" space="2">
+                        <Skeleton h='3' rounded='full' w='50%' startColor='#1f1f1f' />
+                        <HStack space="2" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                    </VStack>
+                </View>
+            }
+
+            {Fetching &&
+                <View className='mb-3 mx-4 flex-row '>
+                    <Skeleton w='24' h='24' rounded='lg' startColor='#1f1f1f' style={{ marginRight: 16 }} />
+                    <VStack flex="3" space="2">
+                        <Skeleton h='3' rounded='full' w='50%' startColor='#1f1f1f' />
+                        <HStack space="2" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                        <HStack space="1" alignItems="center">
+                            <Skeleton size="4" rounded="full" startColor='#1f1f1f' />
+                            <Skeleton h='2' rounded='full' w='70%' startColor='#1f1f1f' />
+                        </HStack>
+                    </VStack>
+                </View>
+            }
+
+            {!Fetching &&
+                NDRestaurants.map((restaurant) =>
+                (
+                    <RestaurantCards
+                        // key={restaurant.id}
+                        // id={restaurant.id}
+                        image={restaurant["image"]}
+                        title={restaurant["name"]}
+                        timing={restaurant["timing"]}
+                        genre={restaurant["genre"]}
+                        location={restaurant["location"]}
+                        description={restaurant["description"]}
+                        dishes={restaurant["dishes"]} />
+                ))
+            }
+        </ScrollView>
+    )
 }
 
 export default Restaurants
