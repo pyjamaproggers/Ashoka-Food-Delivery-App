6a0ea1006ea2572790fdbafb8815146e12c4037a
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-06-27 23:07:05 +0530

    Changed restuarant sanity schema

--- ashoka-food-app/sanity/schemas/restaurant.js
+++ ashoka-food-app/sanity/schemas/restaurant.js
@@ -46,5 +46,23 @@ export default defineType({
       title: 'Dishes',
       of: [{type: 'reference', to: [{type: 'dish'}] }],
     },
+    {
+      name: "Veg_NonVeg",
+      type: 'string',
+      title: 'Is the restaurant veg or non-veg?',
+      validation: (Rule) => Rule.max(100),
+    },
+    {
+      name: "CostForTwo",
+      type: 'string',
+      title: 'Approxiamate cost to fill two people',
+      validation: (Rule) => Rule.max(100),
+    },
+    {
+      name: "RestaurantPhone",
+      type: 'string',
+      title: 'Restaurant Phone Number',
+      validation: (Rule) => Rule.max(100),
+    },
   ]
 })
