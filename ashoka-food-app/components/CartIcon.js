19aa0d14041039ba630cb3fdef16038b4827e60b
iamaryanyadavv <aryanyadav1601@gmail.com>
2023-06-29 16:03:04 +0530

    actionsheet dark mode

--- ashoka-food-app/components/CartIcon.js
+++ ashoka-food-app/components/CartIcon.js
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import { View, TouchableOpacity, Text, useColorScheme, Image, useDisclose, ScrollView } from 'react-native';
+import { View, TouchableOpacity, Text, useColorScheme, Image, useDisclose, ScrollView, Dimensions, SafeAreaView } from 'react-native';
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart, removeFromCart, selectCartItems, selectCartTotal } from "../reduxslices/cartslice";
 import Styles from './Styles';
@@ -8,34 +8,39 @@ import Chevronup from '../assets/chevronupicon.png';
 import Chevrondown from '../assets/chevrondownicon.png';
 import Cart from '../assets/carticon.png';
 import React, { useMemo, useState, useLayoutEffect } from "react";
-import { Actionsheet } from "native-base";
+import { Actionsheet, useColorMode } from "native-base";
 import DishRow from '../screens/DishRow';
+import { color } from '@rneui/base';
 
 export default function CartIcon() {
     const items = useSelector(selectCartItems)
     const cartTotal = useSelector(selectCartTotal)
     const navigation = useNavigation()
     const colorScheme = useColorScheme()
+    const colorMode = useColorMode();
+    const windowHeight = Dimensions.get('window').height;
+    const CartButtonHeight1 = Math.ceil(windowHeight*0.01)
+    const CartButtonHeight2 = Math.ceil(windowHeight*0.0175)
     const [showCartSheet, setShowCartSheet] = useState(false)
     const [groupedItemsInBasket, setGroupedItemsInBasker] = useState([]);
     const dispatch = useDispatch();
     useMemo(() => {
         const groupedItems = items.reduce((results, item) => {
-          (results[items.id] = results[items.id] || []).push(item);
-          return results;
+            (results[items.id] = results[items.id] || []).push(item);
+            return results;
         }, {});
         setGroupedItemsInBasker(groupedItems);
-      }, [items]);
+    }, [items]);
 
     if (items.length === 0) return null
 
     return (
         <>
             {showCartSheet == false &&
-                <View className="absolute bottom-0 w-screen z-20"
+                <SafeAreaView className="absolute bottom-0 w-screen z-20"
                     style={[colorScheme == 'light' ? Styles.LightCartButton : Styles.DarkCartButton]}
                 >
-                    <HStack className='justify-evenly mt-2' style={{ paddingBottom: '10%' }}>
+                    <HStack className='justify-evenly mt-2'>
 
                         <TouchableOpacity
                             onPress={() => setShowCartSheet(!showCartSheet)}
@@ -56,7 +61,7 @@ export default function CartIcon() {
                                 }
                                 {items.length > 1 &&
                                     <Text className='text-md font-semibold text-black'
-                                    style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}
+                                        style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}
                                     >
                                         {items.length} ITEMS ADDED
                                     </Text>
@@ -94,42 +99,44 @@ export default function CartIcon() {
 
                     </HStack >
 
-                </View >
+                </SafeAreaView >
             }
             {showCartSheet == true &&
-                <View>
-                    <Actionsheet isOpen={showCartSheet} onClose={() => { setShowCartSheet(!showCartSheet) }} size="full" 
-                    style={{
-                    }}>
-                        <Actionsheet.Content>
-                            <View className='border-b border-gray-200 w-full'>
+                <SafeAreaView>
+                    <Actionsheet isOpen={showCartSheet} onClose={() => { setShowCartSheet(!showCartSheet) }} size='full'
+                    >
+                        <Actionsheet.Content bgColor={colorMode == 'light' ? "white" : "#262626"} >
+                            <View className='border-b border-gray-200 w-full' style={[colorScheme == 'light' ? Styles.LightBGSec : Styles.DarkBGSec]}>
                                 <Text className=' py-2 pl-2 text-lg font-medium'
-                                style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}>
+                                    style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}>
                                     Items Added
                                 </Text>
                             </View>
-                            <ScrollView className="h-full">
-                            {Object.entries(groupedItemsInBasket).map(([key, items]) => (
-                                items.map((item, index) => (
-                                    <Actionsheet.Item key={`${key}-${index}`}>
-                                        <Text>{item.name}</Text>
-                                        <Text className="text-gray-400">₹{item.Price}</Text>
-                                        <TouchableOpacity>
-                                        <Text
-                                            className="text-[#3E5896] text-xs"
-                                            onPress={() => dispatch(removeFromCart({ id: item.id }))}
-                                        >
-                                            Remove
-                                        </Text>
-                                        </TouchableOpacity>
-                                </Actionsheet.Item>
-                                ))
-                            ))}
+
+                            <ScrollView className="w-full" >
+                                {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+                                    items.map((item, index) => (
+                                        <>
+                                            <Text style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}>{item.name}</Text>
+                                            <Text className="text-gray-400" style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}>₹{item.Price}</Text>
+                                            <TouchableOpacity>
+                                                <Text
+                                                    className="text-[#3E5896] text-xs"
+                                                    onPress={() => dispatch(removeFromCart({ id: item.id }))}
+
+                                                >
+                                                    Remove
+                                                </Text>
+                                            </TouchableOpacity>
+                                        </>
+                                    ))
+                                ))}
+
                             </ScrollView>
-                            <View className="absolute bottom-0 w-screen "
-                                style={[colorScheme == 'light' ? Styles.LightCartButton : Styles.DarkCartButton]}
+
+                            <View className="bottom-0 w-screen"
                             >
-                                <HStack className='justify-evenly mt-2' style={{ paddingBottom: '10%' }}>
+                                <HStack className='justify-evenly mt-2'>
 
                                     <TouchableOpacity
                                         onPress={() => setShowCartSheet(!showCartSheet)}
@@ -141,25 +148,27 @@ export default function CartIcon() {
                                                 style={{ width: 20, height: 20, resizeMode: "contain", }}
                                                 source={Cart}
                                             />
-                                            {items.length == 1 && showCartSheet == true &&
+                                            {items.length == 1 &&
                                                 <Text className='text-md font-semibold text-black'
                                                     style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}
                                                 >
                                                     {items.length} ITEM ADDED
                                                 </Text>
                                             }
-                                            {items.length > 1 && showCartSheet == true &&
-                                                <Text className='text-md font-semibold text-black'>
+                                            {items.length > 1 &&
+                                                <Text className='text-md font-semibold text-black'
+                                                    style={[colorScheme == 'light' ? Styles.LightTextPrimary : Styles.DarkTextPrimary]}
+                                                >
                                                     {items.length} ITEMS ADDED
                                                 </Text>
                                             }
-                                            {showCartSheet && showCartSheet == true &&
+                                            {showCartSheet &&
                                                 <Image
                                                     style={{ width: 12, height: 12, resizeMode: "contain", }}
                                                     source={Chevrondown}
                                                 />
                                             }
-                                            {!showCartSheet && showCartSheet == true &&
+                                            {!showCartSheet &&
                                                 <Image
                                                     style={{ width: 12, height: 12, resizeMode: "contain", }}
                                                     source={Chevronup}
@@ -187,9 +196,10 @@ export default function CartIcon() {
                                 </HStack >
 
                             </View >
+
                         </Actionsheet.Content>
                     </Actionsheet>
-                </View>
+                </SafeAreaView>
             }
 
         </>
